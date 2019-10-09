39c6275ccbda3c5ef3a6b68d7838b6c75931aff6
FullStackFlamingo <15013109+FullStackFlamingo@users.noreply.github.com>
2019-10-09 21:25:50 +0100

    test: jest fixture

---
+++ tests/__fixtures__/ImportStatementFixture.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import Something from 'somewhere';
+
+export default {
+  name: 'ImportStatementFixture',
+  props: {},
+};
+</script>
+
+<style lang="scss">
+@import 'not-an-import-statement';
+</style>
